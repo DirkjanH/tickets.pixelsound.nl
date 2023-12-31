825e972394d18c673840fc3fa5a266d840755e18
Dirkjan Horringa <dirkjan@pellegrina.net>
2023-12-31 21:14:36 +0100

    1e commit

---
+++ instellingen.php
@@ -0,0 +1,39 @@
+<?php
+// Gegevens organisatie
+$organisator 				= 'Trajecti Voces';
+$plaats 					= 'Utrecht';
+$afzender 					= 'reserveren@trajectivoces.net';
+$aanspreekwijze 			= 'jij'; // default: jij;
+$rekeningnummer			    = 'rekening NL90 INGB 0000 2542 86';
+$rekeninghouder			    = 'Trajecti Voces';
+$privacybeleid 			    = 'Privacydocumentenverklaring.php';
+$tijdslot					=	0; // minuten voor volgende boeking met zelfde email
+$aantal_dagen_voor_overboeking = 3;
+$GDPR 						= false; // Geen gegevens kaartbestellers prijsgeven volgens GDPR/AVG
+
+// Gegevens database
+$hostname 					= 'localhost';
+$database 					= 'trajecti_bestel';
+$username 					= 'trajecti_db';
+$password 					= 'Sweelinck1';
+
+// Gegevens tabellen
+$tabel_reserveringen 	    = 'TV_reserveringen';
+$tabel_concerten 			= 'TV_concerten';
+$tabel_CDs 					= 'TV_CDs';
+$tabel_CD_bestellingen 	    = 'TV_CD_bestellingen';
+$dank_pagina 				= "dank_kaartbestelling.php";
+$dank_CD 					= "dank_CD.php";
+$dank_pagina 				= "dank_kaartbestelling.php";
+$webhook                    = "verzend_ticket.php";
+$url                        = "https://trajectivoces.net/bestellen/";
+
+// Gegevens mail
+$mail_host				 	= 'smtp.trajectivoces.net';
+$mail_username			 	= 'horringa';
+$mail_password			 	= 'Dirigent12.';
+
+// Gegevens css
+$css 						= 'css/bestel.css';
+$favicon 					= '../images/logo/favicon.png';
+?>
---
+++ mollie.php
@@ -0,0 +1,7 @@
+<?php 
+require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
+
+$mollie = new \Mollie\Api\MollieApiClient();
+//$mollie->setApiKey(""); // 
+$mollie->setApiKey(""); // 
+?>
