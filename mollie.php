474ddc495595d7feaeed387542341504b8bf0e0a
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-01-22 13:43:23 +0100

    m

--- mollie.php
+++ mollie.php
@@ -1,5 +1,5 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php');
-$mollie = new \Mollie\Api\MollieApiClient();
-//$mollie->setApiKey("live_ADwCgu2bEtGWpCVsRh2Ct9CumyddSd"); // LIVE  voor Festina Lente
+//$mollie = new \Mollie\Api\MollieApiClient();
+$mollie->setApiKey("live_ADwCgu2bEtGWpCVsRh2Ct9CumyddSd"); // LIVE  voor Festina Lente
 $mollie->setApiKey("test_drD7yNgEVNdPAeB2f3xbMJnECFpDaM"); // TEST voor Festina Lente 
